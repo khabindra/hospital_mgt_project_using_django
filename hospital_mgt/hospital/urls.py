842b78dd317b434757fa9803b381e03e41e504ea
khabindra <pujakhabi78@gmail.com>
2023-10-14 06:04:47 +0545

    home contact and about ko link gare in navigation.html

--- hospital_mgt/hospital/urls.py
+++ hospital_mgt/hospital/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import Home,About
+from .views import Home,About,Contact
 
 urlpatterns = [
     path('', Home,name='home'),
     path('about/',About,name='about'),
+    path('contact/',Contact,name='contact'),
 
     
 
