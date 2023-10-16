98d7287d3800759d24e579909742fbed0af4e239
khabindra <pujakhabi78@gmail.com>
2023-10-16 09:56:39 +0545

    login and logout for admin is complete

--- hospital_mgt/hospital/urls.py
+++ hospital_mgt/hospital/urls.py
@@ -1,11 +1,15 @@
 
 from django.urls import path
-from .views import Home,About,Contact
+from .views import Home,About,Contact,Login,Logout_admin
 
 urlpatterns = [
     path('', Home,name='home'),
     path('about/',About,name='about'),
     path('contact/',Contact,name='contact'),
+    path('admin_login/',Login,name='login'),
+    path('logout/',Logout_admin,name='logout_admin'),
+
+
 
     
 
